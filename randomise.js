abc7b74b077612d9ffb56b4259082339be29a2a7
alastairbishop <33623980+alastairbishop@users.noreply.github.com>
2023-12-09 17:44:58 +0000

    Update randomise.js

--- randomise.js
+++ randomise.js
@@ -112,6 +112,7 @@ function chooseVillains(playersConfig, alwaysLeads, ignoreAlwaysLeads, schemeCon
 	} else {
 		enforceAlwaysLeads = !ignoreAlwaysLeads;
 	}
+	console.log(alwaysLeads, ignoreAlwaysLeads, alwaysLeadsOption, enforceAlwaysLeads);
 	if (enforceAlwaysLeads) {
 		include.push(alwaysLeads);
 	}
