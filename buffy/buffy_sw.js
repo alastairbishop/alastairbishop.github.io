fb7c27c6bb2db31c7b0e6d6f3e17fafa906a1f52
bigredapps <33623980+bigredapps@users.noreply.github.com>
2023-12-15 06:53:35 +0000

    Update buffy_sw.js

--- buffy/buffy_sw.js
+++ buffy/buffy_sw.js
@@ -40,7 +40,7 @@ self.addEventListener("fetch", (event) => {
   // when seeking an HTML page
   if (event.request.mode === "navigate") {
     // Return to the index.html page
-    event.respondWith(caches.match("/buffy.html"));
+    event.respondWith(caches.match("/index.html"));
     return;
   }
 
